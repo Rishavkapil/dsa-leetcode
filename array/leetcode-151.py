69c121f51c2f8e9dbe62316dd18e6a93fb932ac0
Rishavkapil <rishavkapil61@gmail.com>
2025-11-26 14:57:34 +0530

    Update Leetcode 151

--- array/leetcode-151.py
+++ array/leetcode-151.py
@@ -8,11 +8,13 @@ continues_space = 0
 rev = ""
 each_word  = "" 
 for ch in name:
-    each_word =  each_word+ ch
-    if(ch == " "):
+    if ch != ' ':
+        each_word =  each_word+ ch
+    else:
+        if(ch == " "):
         # print(each_word, len(each_word))
-        rev = each_word + rev
-        each_word = ""
+            rev = each_word + rev
+            each_word = ""
 # print(each_word, len(each_word))
 
 rev = each_word + " " + rev
