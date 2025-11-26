6a3dc0d015deddf179594c72e820c31c3fc9a5e0
Rishavkapil <rishavkapil61@gmail.com>
2025-11-26 23:36:49 +0530

    Leetcode 1903

--- strings/leetcode-1903.py
+++ strings/leetcode-1903.py
@@ -1,2 +1,17 @@
-num = "134"
-print(type(num))
+num = "52"
+
+# Converting string to integer for easy calculations
+int_num = int(num)
+
+curr_max = -1
+if(int_num % 2!= 0):
+    curr_max = int_num
+while(int_num > 0):
+    lastDigit = int_num % 10
+    int_num = int_num//10
+    if (lastDigit%2!=0 ):
+        curr_max = lastDigit
+    elif(int_num % 2 != 0):
+        curr_max = int_num
+        break
+print("Maximum ODD in the number is : " , curr_max)
