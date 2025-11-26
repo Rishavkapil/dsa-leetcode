92444677cdd934b92bf013f4e7d78964ce0a0ad1
Rishavkapil <rishavkapil61@gmail.com>
2025-11-26 13:11:02 +0530

    Leetcode 151

---
+++ array/leetcode-151.py
@@ -0,0 +1,23 @@
+## First lets try to reverse the nameing normally
+
+
+name = "a good   example"
+n = len(name)
+print(name)
+continues_space = 0
+rev = ""
+each_word  = "" 
+for ch in name:
+    each_word =  each_word+ ch
+    if(ch == " "):
+        # print(each_word, len(each_word))
+        rev = each_word + rev
+        each_word = ""
+# print(each_word, len(each_word))
+
+rev = each_word + " " + rev
+print(rev, "Length of reverse String : " , len(rev))
+
+rev = rev.strip()
+
+print(rev, "Length of reverse String : " , len(rev))
