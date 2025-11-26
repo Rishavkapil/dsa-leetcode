b5e1580005170a4d93c4fd4df5daf3f07c7e4860
Rishavkapil <rishavkapil61@gmail.com>
2025-11-26 15:14:18 +0530

    Updated Leetcode 151 Solution

--- array/leetcode-151.py
+++ array/leetcode-151.py
@@ -1,7 +1,8 @@
 ## First lets try to reverse the nameing normally
 
 
-name = "a good   example"
+# name = " hello world "
+name = " a good   example "
 n = len(name)
 print(name)
 continues_space = 0
@@ -11,15 +12,16 @@ for ch in name:
     if ch != ' ':
         each_word =  each_word+ ch
     else:
-        if(ch == " "):
-        # print(each_word, len(each_word))
-            rev = each_word + rev
-            each_word = ""
+        if each_word:
+            if rev:
+                rev = " " + rev
+        rev = each_word + rev
+        each_word = ""
 # print(each_word, len(each_word))
 
 rev = each_word + " " + rev
-print(rev, "Length of reverse String : " , len(rev))
+print(rev)
 
 rev = rev.strip()
 
-print(rev, "Length of reverse String : " , len(rev))
+print(rev)
