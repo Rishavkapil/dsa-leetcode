c5f7f2ad7e160151c5074b643664cce121272c13
Rishavkapil <rishavkapil61@gmail.com>
2025-12-02 13:20:30 +0530

    Leetcode 169 solution

--- array/leetcode-169.py
+++ array/leetcode-169.py
@@ -1,1 +1,17 @@
 nums = [2,2,1,1,1,2,2]
+
+s = set()
+dc = {}
+# print(type(s))
+
+for i in nums:
+    dc[i] = dc.get(i,0)+1 # this will try to get value for key i.e i , if i is not present it will return 0. 
+
+for i in dc:
+    if(dc[i] > len(nums)//2):
+        # print(dc[i])
+        print(i)
+        break
+print(dc)
+
+
